d0745ad46c30d5816dbd66939f5a03101b53ad26
jorge bejarano <jorge.bejarano@yahoo.com>
2020-04-03 16:04:50 -0500

    first commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinarySearchTree;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
